fbdc048641a18f3251a835bc6221cf4d69f5e452
Eduardo RM <eduardo.ramarti@gmail.com>
2022-12-27 18:04:39 -0600

    EJ2

---
+++ first-node-project/e2.js
@@ -0,0 +1,14 @@
+const dataList = [
+    'Eduardo', 
+    22, 
+    true,
+    birthday = new Date("april 03 2000"),
+    favoriteBook = {
+        'Titulo': 'Why nobody tell me this before?',
+        'Autor':'Dr. Julie Smith',
+        'Fecha': '11 Enero 2022',
+        'URL':'https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjQwdOagZv8AhXukWoFHTG4BmQQFnoECCIQAQ&url=https%3A%2F%2Fwww.amazon.com%2FWhy-Nobody-Told-This-Before%2Fdp%2F0063227932&usg=AOvVaw2n-v6YVj2gSDpvxRS1XdK6'
+        }
+];
+
+console.log(dataList);
