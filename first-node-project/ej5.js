ce06bd4ebd953526d7b622f15bf379d3cccf95ed
Eduardo RM <eduardo.ramarti@gmail.com>
2022-12-28 18:17:41 -0600

    EJ5

---
+++ first-node-project/ej5.js
@@ -0,0 +1,13 @@
+let heightCM = 167;
+let heightM = 1.67;
+
+let weightK = 65.2;
+
+let heightR = Math.round(heightM);
+console.log(heightR)
+
+let weightR = parseInt(weightK);
+console.log(weightR)
+
+let max_valor_JS = Number.MAX_VALUE;
+console.log(`el máximo valor que se puede obtener en Javascript + 1: ${max_valor_JS}`)
